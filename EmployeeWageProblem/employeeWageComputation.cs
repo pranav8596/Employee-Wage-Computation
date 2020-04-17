3f4fbe6f87150efca21cd380d19a0a533e1f157f
pranav8596 <pige786123@gmail.com>
2020-04-17 12:26:50 +0530

    ADDED: Checked if an Employee is Present or Absent

---
+++ EmployeeWageProblem/employeeWageComputation.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace EmployeeWageProblem
+{
+    class EmployeeWageComputation
+    {
+        static void Main(string[] args)
+        {
+            //Constant
+            int IS_FULL_TIME = 1;
+
+            //To get a random number by using Random class
+            Random random = new Random();
+            double empCheck =  Math.Floor(random.NextDouble() * 10) % 2;
+
+            //Check if an Employee is Present or Absent 
+            if (empCheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+
+        }
+    }
+}
