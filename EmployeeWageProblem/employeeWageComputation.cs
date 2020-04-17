48293028639324930bd915433c16f2e1a0590265
pranav8596 <pige786123@gmail.com>
2020-04-17 13:27:39 +0530

    ADDED: Calculated the daily wage of an Employee with Part Time

--- EmployeeWageProblem/employeeWageComputation.cs
+++ EmployeeWageProblem/employeeWageComputation.cs
@@ -2,13 +2,13 @@
 
 namespace EmployeeWageProblem
 {
-     
     class EmployeeWageComputation
     {
         //Constants
         static int IS_FULL_TIME = 1;
+        static int IS_PART_TIME = 2;
         static int EMP_RATE_PER_HOUR = 20;
-        
+
         static void Main(string[] args)
         {
             //Variables
@@ -17,20 +17,21 @@ namespace EmployeeWageProblem
 
             //To get a random number by using Random class
             Random random = new Random();
-            double empCheck =  Math.Floor(random.NextDouble() * 10) % 2;
+            double empCheck = Math.Floor(random.NextDouble() * 10) % 3;
 
             //Check if an Employee is Present or Absent 
             if (empCheck == IS_FULL_TIME)
             {
                 empHours = 8;
-                Console.WriteLine("Employee is Present");
+            }
+            else if (empCheck == IS_PART_TIME)
+            {
+                empHours = 4;
             }
             else
             {
                 empHours = 0;
-                Console.WriteLine("Employee is Absent");
             }
-
             //Calculation of Daily wage
             empWage = empHours * EMP_RATE_PER_HOUR;
             Console.WriteLine("Employee wage: " + empWage);
